c9d28b9d11d3c1786be82be8350783879c840733
CynthiaMichelle9 <129108582+CynthiaMichelle9@users.noreply.github.com>
2024-06-12 12:15:52 -0400

    first commit

---
+++ myapp/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+
+  <Cv Run="22.222.222-2" Apellidos="MUSK" Nombres="ELON REEVE" Nacionalidad="SUDAFRICANO" Sexo="M" FechaNacimiento="28 JUN 1971" NumeroDocumento="222.222.222" FechaEmision="01 ENE 2025" FechaVencimiento="01 ENE 2030"/>
+
+</template>
+
+<script>
+
+import CurriculumVitae from './components/cv.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Cv: CurriculumVitae
+  }
+}
+</script>
+
+<style>
+
+</style>
